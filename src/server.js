a636883b17353fdca23b2d83b17d3fd2af5b5e7f
Faisal <achfaisal96@gmail.com>
2024-10-28 19:44:14 +0700

    change host in server.js

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const init = async () => {
 
   const server = Hapi.server({
     port: 3000,
-    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    host: "0.0.0.0",
     routes: {
       cors: {
         origin: ["*"],
